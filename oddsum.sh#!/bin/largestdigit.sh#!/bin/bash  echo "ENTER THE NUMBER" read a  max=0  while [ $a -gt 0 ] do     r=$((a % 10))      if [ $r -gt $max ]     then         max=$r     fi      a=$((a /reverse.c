827fde6e52833d06996728c6df163ea7661ba4e9
PRIYADHARSHINIV-CSE <priyadharshiniv.cse2025@citchennai.net>
2026-08-21 14:03:24 +0530

    Add reverse number functionality and fix shell script
    Added a C program to reverse a number and fixed formatting issues in the shell script.

---
+++ oddsum.sh#!/bin/largestdigit.sh#!/bin/bash  echo "ENTER THE NUMBER" read a  max=0  while [ $a -gt 0 ] do     r=$((a % 10))      if [ $r -gt $max ]     then         max=$r     fi      a=$((a /reverse.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, remainder, rnum = 0;
+
+    printf("ENTER THE NUMBER: ");
+    scanf("%d", &n);
+
+    while (n != 0) {
+        remainder = n % 10;
+        rnum = rnum * 10 + remainder;
+        n = n / 10;
+    }
+
+    printf("REVERSE OF THE NUMBER IS %d\n", rnum);
+
+    return 0;
+}
